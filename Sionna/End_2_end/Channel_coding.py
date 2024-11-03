e2065b9ca75c09b3bb8640580be7b9fd29b9a85e
kapis20 <kacper.filip.sikorski@gmail.com>
2024-11-03 21:12:05 +0000

    was able to encode and decode. Need to now evaluate it

--- Sionna/End_2_end/Channel_coding.py
+++ Sionna/End_2_end/Channel_coding.py
@@ -1,19 +1,34 @@
 import sionna
 #load required Sionna components 
 from sionna.fec.ldpc.encoding import LDPC5GEncoder
-from sionna.fec.ldpc.decoding import LDPCDecoder
+from sionna.fec.ldpc.decoding import LDPC5GDecoder
 from sionna.channel import AWGN
 
 import tensorflow as tf
+import numpy as np
+
+
 
 
 #LDPC built in Encoder/Decoder Sionna 
 
-#Parameters:
+#Parameters from the paper:
 block_size = 4096 #N, block size 
 code_rate = 3/4 # LDPC code rate
 num_iterations = 50 #BP iterations 
 inf_bits = int(block_size * code_rate) #information bits
+
+#Other parametes 
+#generate random bits with NumPy
+input_bits = np.random.randint(0,2,inf_bits).astype(np.int32)
+#reshape input_bits to have a batch dimensions
+input_bits = input_bits.reshape(1,-1) #shape (1, inf_bits)
+#convert to TensorFlow float 32 (needed for the object in sionna)
+input_bits = tf.convert_to_tensor(input_bits, dtype = tf.float32)
+snr_db = 5 #SNR in dB 
+#Convert to noise power: (needed for AWGN channel in Sionna)
+noise_power = 10** (-snr_db/10)
+
 
 #Initiate encoder and decoder
 encoder = LDPC5GEncoder(
@@ -21,13 +36,27 @@ encoder = LDPC5GEncoder(
     n = block_size #coded bits number
 )
 
-decoder = LDPCDecoder(
-    k = inf_bits, 
-    n = block_size,
+decoder = LDPC5GDecoder(
+    encoder = encoder, #pass encoder instance 
     num_iter = num_iterations, #numper of BP iterations
     hard_out = False #use soft-output decoding 
 )
 
 awgn_channel = AWGN() #init AWGN channel layer 
+
+
+encoded_bits = encoder(input_bits)
+#convert encoded bits to cpmplex Dtype 
+encoded_bits = tf.cast(encoded_bits, dtype = tf.complex64)
+noisy_sig = awgn_channel((encoded_bits,noise_power))
+
+# Convert the noisy signal to float32 (use real part)
+noisy_sig_real = tf.math.real(noisy_sig)
+noisy_sig_real = tf.cast(noisy_sig_real, dtype=tf.float32)
+
+decoded_bits = decoder(noisy_sig_real)
+
+print("Input message is:",input_bits)
+print("Decoded message is:", decoded_bits)
 
 
