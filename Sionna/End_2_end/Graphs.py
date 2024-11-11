34b8131942728d7b10e761891861c89beb6eab05
kapis20 <kacper.filip.sikorski@gmail.com>
2024-11-11 17:52:14 +0000

    updated graphs so single NN BLER/BER still can be plotted

--- Sionna/End_2_end/Graphs.py
+++ Sionna/End_2_end/Graphs.py
@@ -55,6 +55,42 @@ def plot_ber_bler(results_filename,baseline_filename):
     plt.savefig("ber_bler_combined_plot.png")
     plt.show()
 
+#plot only for NN demapper 
+def plot_ber_bler_NN(results_filename):
+    with open(results_filename, 'rb') as f:
+        results = pickle.load(f)
+
+   
+
+        # Extract SNR values, BLER, and BER specifically for 'autoencoder-NN'
+        ebno_dbs = results['ebno_dbs']['autoencoder-NN']
+        BLER = results['BLER']['autoencoder-NN']
+        BER = results['BER']['autoencoder-NN']
+
+        # Create a figure with two subplots, one for BER and one for BLER
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12), sharex=True)
+
+        # Plot BER on the top subplot
+        ax1.semilogy(ebno_dbs, BER, 'x--', c='C1', label='BER - autoencoder-NN')
+        ax1.set_ylabel("BER")
+        ax1.grid(which="both", linestyle='--', linewidth=0.5)
+        ax1.legend()
+        ax1.set_ylim((1e-4, 1.0))
+
+        # Plot BLER on the bottom subplot
+        ax2.semilogy(ebno_dbs, BLER, 'o-', c='C0', label='BLER - autoencoder-NN')
+        ax2.set_xlabel(r"$E_b/N_0$ (dB)")
+        ax2.set_ylabel("BLER")
+        ax2.grid(which="both", linestyle='--', linewidth=0.5)
+        ax2.legend()
+        ax2.set_ylim((1e-4, 1.0))
+
+        
+      
+        # Adjust layout to prevent overlap
+        plt.tight_layout()
+        plt.savefig("ber_bler_NN_plot.png")
+        plt.show()
 
 ###################################################
 #Constellation function
@@ -139,6 +175,7 @@ def plot_loss_function(loss_file_path,fig_file_path):
 ##################################################
 # Plot the BER and BLER results from "bler_results.pkl"
 plot_ber_bler("bler_results.pkl","bler_results_baseline.pkl")
+plot_ber_bler_NN("bler_results.pkl")
 
 # Plot the constellation before training
 plot_constellation("constellation_data.pkl", stage="constellation_before", num_bits_per_symbol=6)
