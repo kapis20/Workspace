82f0359a66d17c5b24ddbc3381207879a8a1bf30
kapis20 <kacper.filip.sikorski@gmail.com>
2024-12-12 22:08:27 +0000

    new model created and debugged

--- Sionna/End_2_end/ConventionalNN.py
+++ Sionna/End_2_end/ConventionalNN.py
@@ -74,10 +74,10 @@ span_in_symbols = 32 # Filter span in symbold
 samples_per_symbol = 4 # Number of samples per symbol, i.e., the oversampling factor
 
 
-BATCH_SIZE = 128#10 #how many examples are processed by sionna in parallel 
+BATCH_SIZE = tf.constant(128, tf.int32) # Training batch size#10 #how many examples are processed by sionna in parallel 
 lenght_of_block = int(num_symbols_per_codeword)
 #Name to store weights 
-model_weights_path = "weights-neural-demapper"
+model_weights_path = "weights-neural-demapper-Conventional"
 ##############################################
 #Evaluation metrics 
 ##############################################
@@ -120,23 +120,25 @@ class NeuralDemapper(Layer): # Inherits from Keras Layer
         super().__init__()
 
         # The three dense layers that form the custom trainable neural network-based demapper
-        self.dense_1 = Dense(64, 'relu')
-        self.dense_2 = Dense(64, 'relu')
-        self.dense_3 = Dense(num_bits_per_symbol, None) # The last layer has no activation and therefore outputs logits, i.e., LLRs
+        self._dense_1 = Dense(128, 'relu')
+        self._dense_2 = Dense(128, 'relu')
+        self._dense_3 = Dense(num_bits_per_symbol, None) # The last layer has no activation and therefore outputs logits, i.e., LLRs
 
-    def call(self, y):
+    def call(self, inputs):
 
-        # y : complex-valued with shape [batch size, block length]
-        # y is first mapped to a real-valued tensor with shape
-        #  [batch size, block length, 2]
-        # where the last dimension consists of the real and imaginary components
-        # The dense layers operate on the last dimension, and treat the inner dimensions as batch dimensions, i.e.,
-        # all the received symbols are independently processed.
-        nn_input = tf.stack([tf.math.real(y), tf.math.imag(y)], axis=-1)
-        z = self.dense_1(nn_input)
-        z = self.dense_2(z)
-        z = self.dense_3(z) # [batch size, number of symbols per block, number of bits per symbol]
-        llr = tf.reshape(z, [tf.shape(y)[0], -1]) # [batch size, number of bits per block]
+        y,no = inputs
+        # Using log10 scale helps with the performance
+        no_db = log10(no)
+        # Stacking the real and imaginary components of the complex received samples
+        # and the noise variance
+        no_db = tf.tile(no_db, [1, num_symbols_per_codeword]) # [batch size, num_symbols_per_codeword]
+
+        z = tf.stack([tf.math.real(y),
+                      tf.math.imag(y),
+                      no_db], axis=2) # [batch size, num_symbols_per_codeword, 3]
+        llr = self._dense_1(z)
+        llr = self._dense_2(llr)
+        llr = self._dense_3(llr) # [batch size, num_symbols_per_codeword, num_bits_per_symbol]
         return llr
     
 
@@ -178,7 +180,7 @@ class End2EndSystem(Model): # Inherits from Keras Model
         self.ds = Downsampling(samples_per_symbol, self.rrcf.length-1, num_symbols_per_codeword) #offset due to group delay
         #Demapper
         self.demapper = NeuralDemapper() # Intantiate the NeuralDemapper custom layer as any other  
-        self.bce = tf.keras.losses.BinaryCrossentropy(from_logits=True) # Loss function
+        #self.bce = tf.keras.losses.BinaryCrossentropy(from_logits=True) # Loss function
 
         self.training = training
 
@@ -199,14 +201,6 @@ class End2EndSystem(Model): # Inherits from Keras Model
                 saturation_amplitude = 0.9,
                 smoothness_factor = 1.93
             )
-
-            
-
-            #######################################
-            # Receiver: 
-            #######################################
-            
-
             self.deinterlever = Deinterleaver(self.interleaver) #pass interlever instance
             self.decoder = LDPC5GDecoder(
                 encoder=self.encoder,       # Pass encoder instance
@@ -214,13 +208,27 @@ class End2EndSystem(Model): # Inherits from Keras Model
                 hard_out=False,             # Use soft-output decoding
                 num_iter=num_iter           # Number of BP iterations
             )
+        # Loss function
+        if self.training:
+            self._bce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+            #######################################
+            # Receiver: 
+            #######################################
+            
+
+          
 
 
     @tf.function(jit_compile=True) # Enable graph execution to speed things up
     def __call__(self, batch_size, ebno_db):
+        #If `ebno_db` is a scalar, a tensor with shape [batch size] is created as it is what is expected by some layers
+        if len(ebno_db.shape) == 0:
+            ebno_db = tf.fill([batch_size], ebno_db)
+
         #noise variance:
         no = ebnodb2no(ebno_db, num_bits_per_symbol,coderate)
-
+        no = expand_to_rank(no, 2)
         #########################
         ###Transmitter 
         ########################
@@ -252,11 +260,11 @@ class End2EndSystem(Model): # Inherits from Keras Model
         x_rrcf = self.rrcf(x_us)#, padding = "same")
 
           ############################
-        # if not self.training:  # Apply Rapp model only in inference mode
-        #    # Rapp noise addition 
-        #     ############################
-        #     x_rrcf = self.RappModel(x_rrcf)
-        #     ############################
+        if not self.training:  # Apply Rapp model only in inference mode
+           # Rapp noise addition 
+            ############################
+            x_rrcf = self.RappModel(x_rrcf)
+            ############################
         #Channel:
         ############################
         y = self.awgn_channel([x_rrcf, no]) #passed symbols to the channel together with noise variance 
@@ -264,17 +272,18 @@ class End2EndSystem(Model): # Inherits from Keras Model
         ############################
         #matched filter, downsampling 
         ############################
-        y_mf = self.m_rrcf(y)#,padding ="same")
+        y_mf = self.m_rrcf(y)
         #y_mf = self.rrcf(y)
-        y_ds = self.ds(y_mf) #downsample sequence
+        y_ds = self.ds(y_mf) 
        
 
         ############################
         #Receiver
         ############################
-        # Demapping 
-        llr = self.demapper(y_ds)  # Call the NeuralDemapper custom layer as any other
         
+        # Demapping 
+        llr = self.demapper([y_ds,no])  # Call the NeuralDemapper custom layer as any other
+        llr = tf.reshape(llr, [batch_size, n]) #Needs to be reshaped to match decoders expected inpt 
      
         ############################
         #Loss or Output
@@ -282,14 +291,13 @@ class End2EndSystem(Model): # Inherits from Keras Model
         #if training mode, then BCE loss function will be returned between binary date and llrs 
         #BCE compares how well demapper can recover the transmitted bits from received symbols 
         if self.training:
-            loss = self.bce(bits, llr)
+            loss = self._bce(bits, llr)
             return loss
         else:
             #Decode llrs
             #tf.print("shape after demapping is:", tf.shape(llr))
             
             llr = self.deinterlever(llr)
-            llr = tf.reshape(llr, [batch_size, n]) #Needs to be reshaped to match decoders expected inpt 
             decoded_bits = self.decoder(llr)
             return uncoded_bits, decoded_bits, x_rrcf, x, y_ds
         
@@ -301,56 +309,73 @@ class End2EndSystem(Model): # Inherits from Keras Model
 ###################################################
 
 # Number of iterations used for training
-NUM_TRAINING_ITERATIONS = 20000 #was used 30000
+NUM_TRAINING_ITERATIONS = 1 #was used 30000
 
 # Set a seed for reproducibility
 tf.random.set_seed(42)
 
-# Track time for model training
-training_start_time = time.time()
+
+
+# # Instantiating the end-to-end model for training
+# model_train = End2EndSystem(training=True)
+
+# # Extract and save constellation data before training
+# constellation_data['constellation_before']= model_train.constellation.points.numpy()
+
+def conventional_training(model_train):
+    # Adam optimizer (SGD variant)
+    optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+
+    # Training loop
+    for i in range(NUM_TRAINING_ITERATIONS):
+        # Sampling a batch of SNRs
+        ebno_db = tf.random.uniform(shape=[BATCH_SIZE], minval=ebno_db_min, maxval=ebno_db_max)
+    
+        # Forward pass
+        with tf.GradientTape() as tape:
+            loss = model_train(BATCH_SIZE, ebno_db)
+            #The model is assumed to return the BMD rate
+            #store the current loss value 
+            loss_values.append(loss.numpy())
+        # Computing and applying gradients
+        weights = model_train.trainable_weights
+        grads = tape.gradient(loss, model_train.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model_train.trainable_weights))
+        # Print progress
+        if i % 100 == 0:
+            print(f"{i}/{NUM_TRAINING_ITERATIONS}  Loss: {loss:.2E}", end="\r")
+
+
+def save_weights(model_train, model_weights_path):
+    weights = model_train.get_weights()
+    with open(model_weights_path, 'wb') as f:
+        pickle.dump(weights, f)
 
 # Instantiating the end-to-end model for training
 model_train = End2EndSystem(training=True)
-
 # Extract and save constellation data before training
 constellation_data['constellation_before']= model_train.constellation.points.numpy()
-
-
-# Adam optimizer (SGD variant)
-optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
-
-# Training loop
-for i in range(NUM_TRAINING_ITERATIONS):
-
-   
-    # Forward pass
-    with tf.GradientTape() as tape:
-        loss = model_train(BATCH_SIZE, 6.5)#6.0) #training Eb/No set to 6dB, get loss function for the most optimized under 6dB 
-        #The model is assumed to return the BMD rate
-        #store the current loss value 
-        loss_values.append(loss.numpy())
-    # Computing and applying gradients
-    grads = tape.gradient(loss, model_train.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model_train.trainable_weights))
-    # Print progress
-    if i % 100 == 0:
-        print(f"{i}/{NUM_TRAINING_ITERATIONS}  Loss: {loss:.2E}", end="\r")
-
+# Track time for model training
+training_start_time = time.time()
+conventional_training(model_train)
+# Save weights
+save_weights(model_train, model_weights_path)
+# Extract and save constellation data after training
+constellation_data['constellation_after'] = model_train.constellation.points.numpy()
 # End time for training
 training_end_time = time.time()
 
-# Extract and save constellation data after training
-constellation_data['constellation_after'] = model_train.constellation.points.numpy()
+
 
 # Save constellation data to a .pkl file
 with open("constellation_dataNN.pkl", "wb") as f:
     pickle.dump(constellation_data, f)
 
 
-# Save the weightsin a file
-weights = model_train.get_weights()
-with open(model_weights_path, 'wb') as f:
-    pickle.dump(weights, f)
+# # Save the weightsin a file
+# weights = model_train.get_weights()
+# with open(model_weights_path, 'wb') as f:
+#     pickle.dump(weights, f)
 
 # Save loss function values to a file
 with open("loss_values.pkl","wb") as f:
@@ -359,7 +384,9 @@ with open("loss_values.pkl","wb") as f:
 #model evaluation 
 ##################################################
 
-
+##################################################
+# RUN model
+##################################################
 
 # Define the SNR range for evaluation
 ebno_dbs = np.arange(ebno_db_min, ebno_db_max, 0.5)
@@ -375,7 +402,26 @@ def load_weights(model, model_weights_path):
 model = End2EndSystem(training=False) #End2EndSystem model to run on the previously generated weights 
 load_weights(model, model_weights_path)
 
-selected_ebno_dbs = [6.5]  # Adjust as needed
+###################################################
+# SNR
+###################################################
+
+ber_NN, bler_NN = sim_ber(
+    model, ebno_dbs, batch_size=BATCH_SIZE, num_target_block_errors=1, max_mc_iter=1,soft_estimates=True) #was used 1000 and 10000
+    #soft estimates added for demapping 
+results['BLER']['autoencoder-NN'] = bler_NN.numpy()
+results['BER']['autoencoder-NN'] = ber_NN.numpy()
+
+# Save the results to a file (optional)
+with open("bler_resultsNN_conv.pkl", 'wb') as f:
+    pickle.dump((results), f)
+
+
+##############################################
+# Signals from the model
+# ########################################### 
+
+selected_ebno_dbs = [9]  # Adjust as needed
 # Evaluate model and collect signals
 for ebno_db in selected_ebno_dbs:
     # Forward pass through the model
@@ -387,18 +433,10 @@ for ebno_db in selected_ebno_dbs:
 
 print("All selected Eb/N0 evaluations completed.")
 
-ber_NN, bler_NN = sim_ber(
-    model, ebno_dbs, batch_size=BATCH_SIZE, num_target_block_errors=1, max_mc_iter=1,soft_estimates=True) #was used 1000 and 10000
-    #soft estimates added for demapping 
-results['BLER']['autoencoder-NN'] = bler_NN.numpy()
-results['BER']['autoencoder-NN'] = ber_NN.numpy()
 
-# Save the results to a file (optional)
-with open("bler_resultsNN.pkl", 'wb') as f:
-    pickle.dump((results), f)
 
 # Save the x_rrcf signals to a file (as NumPy or TF tensors)
-signal_file = "x_rrcf_signals_no_clippingNN.pkl"
+signal_file = "x_rrcf_signals_no_clippingNN_conv.pkl"
 with open(signal_file, "wb") as f:
     x_rrcf_numpy = {ebno_db: x.numpy() for ebno_db, x in x_rrcf_signals.items()}  # Convert to NumPy for storage
     pickle.dump(x_rrcf_numpy, f)
@@ -409,21 +447,21 @@ with open(signal_file, "wb") as f:
 #     x_rrcf_Rapp_numpy = {ebno_db: x.numpy() for ebno_db, x in x_rrcf_Rapp_signals.items()}  # Convert to NumPy for storage
 #     pickle.dump(x_rrcf_Rapp_numpy, f)
 
-signal_mapperFile = "x_mapperNN.pkl"
+signal_mapperFile = "x_mapperNN_conv.pkl"
 with open(signal_mapperFile, "wb") as f:
     x_mapper = {ebno_db: x.numpy() for ebno_db, x in bits_after_mapper.items()}  # Convert to NumPy for storage
     pickle.dump(x_mapper, f)
 
 
-signal_demapperFile = "y_demapperNN.pkl"
+signal_demapperFile = "y_demapperNN_conv.pkl"
 with open(signal_demapperFile, "wb") as f:
     y_demapper = {ebno_db: x.numpy() for ebno_db, x in bits_before_demapper.items()}  # Convert to NumPy for storage
     pickle.dump(y_demapper, f)
 
 
 
-Time calculations: 
-Calculate and print total execution time
+# Time calculations: 
+# Calculate and print total execution time
 end_time = time.time()
 total_execution_time = end_time - start_time
 training_execution_time = training_end_time - training_start_time
